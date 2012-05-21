aa2d48ad162caf37ed1df5e8e277124c906d0dad
Henry Yan <yanhonglei@gmail.com>
2012-05-22 00:41:37 +0800

    complete leave dao unit test

---
+++ src/main/java/me/kafeitu/demo/activiti/entity/oa/Leave.java
@@ -0,0 +1,110 @@
+package me.kafeitu.demo.activiti.entity.oa;
+
+import java.io.Serializable;
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+import me.kafeitu.demo.activiti.entity.IdEntity;
+
+/**
+ * Entity: Leave
+ *  
+ * @author HenryYan
+ */
+@Entity
+@Table(name = "OA_LEAVE")
+public class Leave extends IdEntity implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+	private String userId;
+	private Date startTime;
+	private Date endTime;
+	private Date realityStartTime;
+	private Date realityEndTime;
+	private Date applyTime;
+	private String leaveType;
+	private String reason;
+
+	@Column
+	public String getUserId() {
+		return userId;
+	}
+
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+
+	@Temporal(TemporalType.TIMESTAMP)
+	@Column(name = "START_TIME")
+	public Date getStartTime() {
+		return startTime;
+	}
+
+	public void setStartTime(Date startTime) {
+		this.startTime = startTime;
+	}
+
+	@Temporal(TemporalType.TIMESTAMP)
+	@Column(name = "END_TIME")
+	public Date getEndTime() {
+		return endTime;
+	}
+
+	public void setEndTime(Date endTime) {
+		this.endTime = endTime;
+	}
+
+	@Column
+	@Temporal(TemporalType.TIMESTAMP)
+	public Date getApplyTime() {
+		return applyTime;
+	}
+
+	public void setApplyTime(Date applyTime) {
+		this.applyTime = applyTime;
+	}
+
+	@Column
+	public String getLeaveType() {
+		return leaveType;
+	}
+
+	public void setLeaveType(String leaveType) {
+		this.leaveType = leaveType;
+	}
+
+	@Column
+	public String getReason() {
+		return reason;
+	}
+
+	public void setReason(String reason) {
+		this.reason = reason;
+	}
+
+	@Temporal(TemporalType.TIMESTAMP)
+	@Column(name = "REALITY_START_TIME")
+	public Date getRealityStartTime() {
+		return realityStartTime;
+	}
+
+	public void setRealityStartTime(Date realityStartTime) {
+		this.realityStartTime = realityStartTime;
+	}
+
+	@Temporal(TemporalType.TIMESTAMP)
+	@Column(name = "REALITY_END_TIME")
+	public Date getRealityEndTime() {
+		return realityEndTime;
+	}
+
+	public void setRealityEndTime(Date realityEndTime) {
+		this.realityEndTime = realityEndTime;
+	}
+
+}
