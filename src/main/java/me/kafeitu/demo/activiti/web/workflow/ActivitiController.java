4a25dc199990e687ca26a95ae876426d0a0b49e3
Henry Yan <yanhonglei@gmail.com>
2012-05-23 22:02:54 +0800

    完成查看流程定义的xml和png功能

--- src/main/java/me/kafeitu/demo/activiti/web/workflow/ActivitiController.java
+++ src/main/java/me/kafeitu/demo/activiti/web/workflow/ActivitiController.java
@@ -1,15 +1,21 @@
 package me.kafeitu.demo.activiti.web.workflow;
 
+import java.io.InputStream;
 import java.util.List;
+
+import javax.servlet.http.HttpServletResponse;
 
 import me.kafeitu.demo.activiti.service.WorkflowProcessDefinitionService;
 
 import org.activiti.engine.RepositoryService;
+import org.activiti.engine.RuntimeService;
 import org.activiti.engine.repository.ProcessDefinition;
+import org.activiti.engine.runtime.ProcessInstance;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 /**
@@ -25,6 +31,12 @@ public class ActivitiController {
 
 	protected RepositoryService repositoryService;
 
+	protected RuntimeService runtimeService;
+
+	/**
+	 * 流程定义列表
+	 * @return
+	 */
 	@RequestMapping(value = "/process-list", method = RequestMethod.GET)
 	public ModelAndView processList() {
 		ModelAndView mav = new ModelAndView("workflow/process-list");
@@ -33,10 +45,55 @@ public class ActivitiController {
 		return mav;
 	}
 
+	/**
+	 * 部署全部流程
+	 * @return
+	 * @throws Exception
+	 */
 	@RequestMapping(value = "/redeploy/all", method = RequestMethod.GET)
 	public String redeployAll() throws Exception {
 		workflowProcessDefinitionService.deployAllFromClasspath();
 		return "redirect:/workflow/process-list";
+	}
+
+	/**
+	 * 读取资源，通过部署ID
+	 */
+	@RequestMapping(value = "/resource/deployment", method = RequestMethod.GET)
+	public void loadByDeployment(@RequestParam("deploymentId") String deploymentId,
+			@RequestParam("resourceName") String resourceName, HttpServletResponse response) throws Exception {
+		InputStream resourceAsStream = repositoryService.getResourceAsStream(deploymentId, resourceName);
+		byte[] b = new byte[1024];
+		int len = -1;
+		while ((len = resourceAsStream.read(b, 0, 1024)) != -1) {
+			response.getOutputStream().write(b, 0, len);
+		}
+	}
+
+	/**
+	 * 读取资源，通过流程ID
+	 */
+	@RequestMapping(value = "/resource/process-instance", method = RequestMethod.GET)
+	public void loadByProcessInstance(@RequestParam("resourceType") String resourceType,
+			@RequestParam("processInstanceId") String processInstanceId, HttpServletResponse response) throws Exception {
+		InputStream resourceAsStream = null;
+		ProcessInstance processInstance = runtimeService.createProcessInstanceQuery().processInstanceId(processInstanceId)
+				.singleResult();
+		ProcessDefinition singleResult = repositoryService.createProcessDefinitionQuery()
+				.processDefinitionId(processInstance.getProcessDefinitionId()).singleResult();
+
+		String resourceName = "";
+		if (resourceType.equals("image")) {
+			resourceName = singleResult.getDiagramResourceName();
+		} else if (resourceType.equals("xml")) {
+			resourceName = singleResult.getResourceName();
+		}
+		resourceAsStream = repositoryService.getResourceAsStream(singleResult.getDeploymentId(), resourceName);
+		byte[] b = new byte[1024];
+		int len = -1;
+		while ((len = resourceAsStream.read(b, 0, 1024)) != -1) {
+			response.getOutputStream().write(b, 0, len);
+		}
 	}
 
 	@Autowired
@@ -47,6 +104,10 @@ public class ActivitiController {
 	@Autowired
 	public void setRepositoryService(RepositoryService repositoryService) {
 		this.repositoryService = repositoryService;
+	}
+
+	public void setRuntimeService(RuntimeService runtimeService) {
+		this.runtimeService = runtimeService;
 	}
 
 }
--- src/main/webapp/WEB-INF/views/workflow/process-list.jsp
+++ src/main/webapp/WEB-INF/views/workflow/process-list.jsp
@@ -14,20 +14,16 @@
 	<%@ include file="/common/include-base-styles.jsp" %>
 	<%@ include file="/common/include-jquery-ui-theme.jsp" %>
 	<title>流程列表</title>
-	
-	<script src="${ctx }/js/common/jquery.js" type="text/javascript"></script>
-	<script src="${ctx }/js/common/plugins/jui/jquery-ui.min.js" type="text/javascript"></script>
-	<script src="${ctx }/js/common/plugins/blockui/jquery.blockUI.js" type="text/javascript"></script>
-	<script src="${ctx }/js/module/activiti/process-list.js" type="text/javascript"></script>
 </head>
 <body>
-	<div style="text-align: center;padding: 2px 0 2px">
-		<a id='redeploy' href='${ctx }/workflow/redeploy/all'>重新部署</a>
+	<div style="text-align: right;padding: 2px 1em 2px">
+		<a id='redeploy' href='${ctx }/workflow/redeploy/all'>重新部署流程</a>
 	</div>
 	<table width="100%" class="need-border">
 		<thead>
 			<tr>
 				<th>ID</th>
+				<th>DID</th>
 				<th>名称</th>
 				<th>KEY</th>
 				<th>版本号</th>
@@ -39,11 +35,12 @@
 			<c:forEach items="${processes }" var="process">
 				<tr>
 					<td>${process.id }</td>
+					<td>${process.deploymentId }</td>
 					<td>${process.name }</td>
 					<td>${process.key }</td>
 					<td>${process.version }</td>
-					<td>${process.resourceName }</td>
-					<td>${process.diagramResourceName }</td>
+					<td><a target="_blank" href='${ctx }/workflow/resource/deployment?deploymentId=${process.deploymentId}&resourceName=${process.resourceName }'>${process.resourceName }</a></td>
+					<td><a target="_blank" href='${ctx }/workflow/resource/deployment?deploymentId=${process.deploymentId}&resourceName=${process.diagramResourceName }'>${process.diagramResourceName }</a></td>
 				</tr>
 			</c:forEach>
 		</tbody>
