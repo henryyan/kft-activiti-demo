9535a7f702b4d8b80b4795287961a87941b7d696
Henry Yan <yanhonglei@gmail.com>
2012-05-31 08:44:20 +0800

    更新用户权限

--- src/main/resources/sql/h2/data.sql
+++ src/main/resources/sql/h2/data.sql
@@ -3,9 +3,12 @@ insert into ACT_ID_GROUP values ('user', 1, '用户', 'security-role');
 insert into ACT_ID_GROUP values ('deptLeader', 1, '部门领导', 'assignment');
 insert into ACT_ID_GROUP values ('hr', 1, '人事', 'assignment');
 
+insert into ACT_ID_USER values ('admin', 1, 'Admin', 'Kad', 'admin@kad.com', '000000', '');
+insert into ACT_ID_MEMBERSHIP values ('admin', 'user');
+insert into ACT_ID_MEMBERSHIP values ('admin', 'admin');
+
 insert into ACT_ID_USER values ('kafeitu', 1, 'Henry', 'Yan', 'yanhonglei@gmail.com', '000000', '');
 insert into ACT_ID_MEMBERSHIP values ('kafeitu', 'admin');
-insert into ACT_ID_MEMBERSHIP values ('kafeitu', 'user');
 
 insert into ACT_ID_USER values ('hruser', 1, 'Lili', 'Zhang', 'hr@gmail.com', '000000', '');
 insert into ACT_ID_MEMBERSHIP values ('hruser', 'user');
