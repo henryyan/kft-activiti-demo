5ce894e4b9b5ff2e5dcd57ac1aec02b3b118973f
Henry Yan <yanhonglei@gmail.com>
2012-08-16 20:53:36 +0800

    合并到no-maven分支脚本添加复制测试类的定义

--- bin/merge-to-no-maven.sh
+++ bin/merge-to-no-maven.sh
@@ -7,6 +7,7 @@ rm $NO_MAVEN_DIR/WebContent/WEB-INF/lib/*
 
 echo "delete java"
 rm -rf $NO_MAVEN_DIR/src/*
+rm -rf $NO_MAVEN_DIR/test/*
 
 echo "delete java resources"
 rm -rf $NO_MAVEN_DIR/resources/*
@@ -24,9 +25,11 @@ cp ./target/dependency/* $NO_MAVEN_DIR/WebContent/WEB-INF/lib/
 
 echo "copying java"
 cp -r src/main/java/* $NO_MAVEN_DIR/src
+cp -r src/test/java/* $NO_MAVEN_DIR/test
 
 echo "copying java resources"
 cp -r src/main/resources/* $NO_MAVEN_DIR/resources
+cp target/classes/application.properties $NO_MAVEN_DIR/resources
 
 echo "copying web resources"
 cp -r src/main/webapp/common $NO_MAVEN_DIR/WebContent/common
@@ -34,3 +37,4 @@ cp -r src/main/webapp/css $NO_MAVEN_DIR/WebContent/css
 cp -r src/main/webapp/images $NO_MAVEN_DIR/WebContent/images
 cp -r src/main/webapp/js $NO_MAVEN_DIR/WebContent/js
 cp -r src/main/webapp/WEB-INF/views $NO_MAVEN_DIR/WebContent/WEB-INF/views
+cp src/main/webapp/WEB-INF/*.xml $NO_MAVEN_DIR/WebContent/WEB-INF
