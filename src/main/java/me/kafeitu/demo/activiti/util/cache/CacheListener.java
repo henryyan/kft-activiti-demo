baf95a573fb4067b0296321656ea57797b9959d4
Henry Yan <yanhonglei@gmail.com>
2013-03-10 13:09:12 +0800

    添加processdefinition的cache

---
+++ src/main/java/me/kafeitu/demo/activiti/util/cache/CacheListener.java
@@ -0,0 +1,58 @@
+package me.kafeitu.demo.activiti.util.cache;
+
+import org.activiti.engine.impl.persistence.entity.ProcessDefinitionEntity;
+import org.infinispan.notifications.Listener;
+import org.infinispan.notifications.cachelistener.annotation.CacheEntryCreated;
+import org.infinispan.notifications.cachelistener.annotation.CacheEntryEvicted;
+import org.infinispan.notifications.cachelistener.annotation.CacheEntryModified;
+import org.infinispan.notifications.cachelistener.annotation.CacheEntryRemoved;
+import org.infinispan.notifications.cachelistener.event.CacheEntryCreatedEvent;
+import org.infinispan.notifications.cachelistener.event.CacheEntryEvictedEvent;
+import org.infinispan.notifications.cachelistener.event.CacheEntryModifiedEvent;
+import org.infinispan.notifications.cachelistener.event.CacheEntryRemovedEvent;
+import org.infinispan.notifications.cachemanagerlistener.annotation.CacheStarted;
+import org.infinispan.notifications.cachemanagerlistener.annotation.CacheStopped;
+import org.infinispan.notifications.cachemanagerlistener.event.CacheStartedEvent;
+import org.infinispan.notifications.cachemanagerlistener.event.CacheStoppedEvent;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * @author Joram Barrez
+ */
+@Listener
+public class CacheListener {
+  
+  private static final Logger logger = LoggerFactory.getLogger(CacheListener.class);
+  
+  @CacheStarted
+  public void cacheStarted(CacheStartedEvent event) {
+    logger.info("Distributed cache started");
+  }
+  
+  @CacheStopped
+  public void cacheStopped(CacheStoppedEvent event) {
+    logger.info("Distributed cache stopped");
+  }
+  
+  @CacheEntryModified
+  public void cacheEntryModified(CacheEntryModifiedEvent<String, ProcessDefinitionEntity> event) {
+    logger.info("Cache entry with key '" + event.getKey() + " modified in cache (local cache size = " + event.getCache().size() + ")");
+  }
+  
+  @CacheEntryCreated
+  public void cacheEntryCreated(CacheEntryCreatedEvent<String, ProcessDefinitionEntity> event) {
+    logger.info("Cache entry with key '" + event.getKey() + " added to cache (local cache size = " + event.getCache().size() + ")");
+  }
+  
+  @CacheEntryRemoved
+  public void cacheEntryRemoved(CacheEntryRemovedEvent<String, ProcessDefinitionEntity> event) {
+    logger.info("Cache entry with key '" + event.getKey() + " removed from cache (local cache size = " + event.getCache().size() + ")");
+  }
+  
+  @CacheEntryEvicted
+  public void cacheEntryEvicted(CacheEntryEvictedEvent<String, ProcessDefinitionEntity> event) {
+    logger.info("Cache entry with key '" + event.getKey() + " evicted from cache (local cache size = " + event.getCache().size() + ")");
+  }
+  
+}
---
+++ src/main/java/me/kafeitu/demo/activiti/util/cache/DistributedCache.java
@@ -0,0 +1,42 @@
+package me.kafeitu.demo.activiti.util.cache;
+
+import org.activiti.engine.impl.persistence.deploy.DeploymentCache;
+import org.activiti.engine.impl.persistence.entity.ProcessDefinitionEntity;
+import org.infinispan.Cache;
+import org.infinispan.manager.CacheContainer;
+import org.infinispan.manager.DefaultCacheManager;
+
+/**
+ * @author Joram Barrez
+ */
+public class DistributedCache implements DeploymentCache<ProcessDefinitionEntity> {
+
+  protected Cache<String, ProcessDefinitionEntity> cache;
+
+  public DistributedCache() {
+    try {
+      CacheContainer manager = new DefaultCacheManager("inifispan-cfg.xml");
+      this.cache = manager.getCache();
+      this.cache.addListener(new CacheListener());
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+  }
+
+  public ProcessDefinitionEntity get(String id) {
+    return cache.get(id);
+  }
+
+  public void add(String id, ProcessDefinitionEntity processDefinitionEntity) {
+    cache.put(id, processDefinitionEntity);
+  }
+
+  public void remove(String id) {
+    cache.remove(id);
+  }
+
+  public void clear() {
+    cache.clear();
+  }
+
+}
