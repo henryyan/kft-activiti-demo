319fc4142ba665f72b622678a5efb0d9ceb39c8b
Henry Yan <yanhonglei@gmail.com>
2013-04-17 13:33:10 +0800

    使用自定义的Activiti版本 5.12.1-kft，添加全局监听演示

---
+++ src/me/kafeitu/demo/activiti/activiti/listener/GlobalTaskListener.java
@@ -0,0 +1,21 @@
+package me.kafeitu.demo.activiti.activiti.listener;
+
+import org.activiti.engine.delegate.DelegateTask;
+import org.activiti.engine.delegate.TaskListener;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * User: henryyan
+ */
+public class GlobalTaskListener implements TaskListener {
+
+  protected Logger logger = LoggerFactory.getLogger(getClass());
+
+  @Override
+  public void notify(DelegateTask delegateTask) {
+    logger.debug("触发了全局监听器, pid={}, tid={}, event={}", new Object[]{
+            delegateTask.getProcessInstanceId(), delegateTask.getId(), delegateTask.getEventName()
+    });
+  }
+}
