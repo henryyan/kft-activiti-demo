6e5c721c62c0a64700493118205d64d1cd0fe7aa
Henry Yan <yanhonglei@gmail.com>
2014-06-13 14:58:59 +0800

    使用内存数据库测试dubbo

--- src/main/java/me/kafeitu/dubbo/Consumer.java
+++ src/main/java/me/kafeitu/dubbo/Consumer.java
@@ -12,13 +12,20 @@ public class Consumer {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(new String[] {"dubbo/consumer.xml"});
         context.start();
 
+        // 读取引擎属性
         ManagementService managementService = (ManagementService)context.getBean("managementService"); // 获取远程服务代理
         Map<String, String> properties = managementService.getProperties();
+        System.out.println( properties ); // 显示调用结果
 
+        // 部署流程(有异常)
+        /*RepositoryService repositoryService = (RepositoryService) context.getBean("repositoryService");
+        String processFilePath = Consumer.class.getClassLoader().getResource("diagrams/leave/leave.bpmn").getPath();
+        FileInputStream inputStream = new FileInputStream(processFilePath);
+        repositoryService.createDeployment().addInputStream("leave.bpmn20.xml", inputStream).deploy();*/
+
+        // 查询流程（有异常）
         RuntimeService runtimeService = context.getBean(RuntimeService.class);
         System.out.println(runtimeService.createExecutionQuery().list());
-
-        System.out.println( properties ); // 显示调用结果
     }
  
 }
--- src/main/java/me/kafeitu/dubbo/Provider.java
+++ src/main/java/me/kafeitu/dubbo/Provider.java
@@ -5,7 +5,7 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class Provider {
  
     public static void main(String[] args) throws Exception {
-        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(new String[] {"applicationContext.xml", "dubbo/provider.xml"});
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(new String[] {"dubbo/applicationContext.xml", "dubbo/provider.xml"});
         context.start();
  
         System.in.read(); // 按任意键退出
